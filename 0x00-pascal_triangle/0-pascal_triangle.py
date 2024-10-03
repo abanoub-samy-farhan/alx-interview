13a7b483431d459e51d194c6e358ed4e6e3c8729
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-10-03 04:58:46 +0300

    list comperhension added

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
+    """This is a function for making pascal's triangle in a form of lists
+    Args:
+        - n: the number of rows in the triangle
+    Returns:
+        - list of lists in case of n > 0
+        - empty list in case of n <= 0
+    """
     pascal = []
     if n <= 0:
         return pascal
     for i in range(n):
-        pre = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                pre.append(1)
-                continue
-            else:
-                pre.append(pascal[i - 1][j] + pascal[i - 1][j - 1])
+        pre = [(1 if j == 0 or j == i else pascal[i - 1][j] + pascal[i - 1][j - 1]) for j in range(i + 1)]
         pascal.append(pre)
 
     return pascal
