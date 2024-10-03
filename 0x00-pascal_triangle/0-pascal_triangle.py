abacc291fdbd788a628ee5f0843430f944cb8632
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-10-03 05:00:54 +0300

    pycodestyle checked

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 pascal's triangle
 """
 
+
 def pascal_triangle(n):
     """This is a function for making pascal's triangle in a form of lists
     Args:
@@ -15,7 +16,9 @@ def pascal_triangle(n):
     if n <= 0:
         return pascal
     for i in range(n):
-        pre = [(1 if j == 0 or j == i else pascal[i - 1][j] + pascal[i - 1][j - 1]) for j in range(i + 1)]
+        pre = [(1 if j == 0 or j == i else
+                pascal[i - 1][j] + pascal[i - 1][j - 1])
+               for j in range(i + 1)]
         pascal.append(pre)
 
     return pascal
