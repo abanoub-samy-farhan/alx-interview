27f26c6fc1483ca2aae927dba9a322720135d48e
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-10-18 00:12:03 +0300

    minimum oeprations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Minimum operations"""
+
+
+def minOperations(n):
+    """Minimum operations"""
+    if n < 2:
+        return 0
+    i = 2
+    operations = 0
+    while i <= n:
+        if n % i == 0:
+            n = n / i
+            operations += i
+        else:
+            i += 1
+    
+    return operations
